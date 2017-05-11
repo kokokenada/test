bb5e373fc9b8f237373c65536d3a64050920f4ae
Ken Ono <ken.ono.gm@gmail.com>
2017-05-10 22:26:48 -0400

    fix server crash on deleting session

--- server/main.ts
+++ server/main.ts
@@ -1,36 +1,40 @@
 import { Meteor } from 'meteor/meteor';
-import {IIPSession} from '../imports/ip-redux-package/ip-types';
+import { IIPSession } from '../imports/ip-redux-package/ip-types';
 
 Meteor.startup(() => {
 
-  let sessions = new Meteor.Collection('sessions');
+  let sessions = new Mongo.Collection('sessions');
   // Server starting so delete any sessions from the past
   sessions.remove({});
 
   // code to run on server at startup
 
+  // Delete must run in right context, else crash
+  let deleteFunction = Meteor.bindEnvironment( (id: string) => {
+    sessions.remove({id: id});
+  });
 
   Meteor.publish("sessions", function() {
-    this._session.socket.on("close", function() {
-      console.log('done session');
-      console.log(this);
-      let ip = this._session.remoteAddress;
-      console.log(ip)
-      sessions.remove({clientAddress: ip});
-    });
+    const doneFunction = (x:any) => {
+
+      let ip = this._session.id;
+//      console.log('done session this._sessison.id ');
+//      console.log(ip)
+      deleteFunction(ip);
+    };
+    this._session.socket.on("close", doneFunction);
     return sessions.find();
   });
 
-  Meteor.onConnection( (data)=> {
+  Meteor.onConnection( (data: any)=> {
 
     const ipSession: IIPSession = {
       id: data.id,
       clientAddress: data.clientAddress,
       start: new Date()
     };
-    console.log('data!');
-    console.log(data);
-    console.log(ipSession);
+//    console.log('data!');
+//    console.log(ipSession);
     sessions.insert(ipSession);
   })
 });
